19d9859fb24bc5293df178afdc8af53a411c0220
Logan <loges_don@icloud.com>
2026-08-16 09:53:09 +1000

    ch13: displaying errors in the form component in create-form.tsx, finalised server side form validation for createInvoice in actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -51,21 +51,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
         };
     }
 
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    // Insert data into the database
     try {
         await sql`
             INSERT INTO invoices (customer_id, amount, status, date)
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
-        console.error(error)
+        // If a database error occurs, return a more specific error
         return {
             message: 'Database Error: Failed to create Invoice.'
         }
     }
 
+    // revalidate the cache for the invoices page and redirect the user
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
