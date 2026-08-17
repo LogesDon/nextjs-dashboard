16f1b753d21b40f4c49f39dc2660312a48bd8c16
Logan <loges_don@icloud.com>
2026-08-18 09:31:52 +1000

    ch13: one more message to implement

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -95,6 +95,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -110,6 +111,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="paid"
@@ -119,8 +121,27 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+            {/* Pretty sure the aria labels for invoice status errors goes here */}
           </div>
         </fieldset>
+        <div id="invoice-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.status &&
+            state.errors.status.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+        </div>
+        {/* FIX THIS */}
+        <div id="invoice-error" aria-live="polite" aria-atomic="true">
+          {state.errors &&
+            state.errors.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+        </div>
+        
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
